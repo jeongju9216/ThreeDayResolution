dd7fa53d77bae886d2cb49964c96197079df4116
jeong9216 <dbwjdwn9216@gmail.com>
2022-01-06 21:09:03 +0900

    Launch Screen

--- ThreeDayHump/ThreeDayHump/ViewControllers/GoalViewController.swift
+++ ThreeDayHump/ThreeDayHump/ViewControllers/GoalViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalViewController: UIViewController {
 
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
