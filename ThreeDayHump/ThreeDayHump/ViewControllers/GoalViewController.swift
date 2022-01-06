ac30fff7cf438c2f7af29e3141f411379610fa27
jeong9216 <dbwjdwn9216@gmail.com>
2022-01-06 23:13:52 +0900

    Goal Screen 진행 중

--- ThreeDayHump/ThreeDayHump/ViewControllers/GoalViewController.swift
+++ ThreeDayHump/ThreeDayHump/ViewControllers/GoalViewController.swift
@@ -9,12 +9,37 @@ import UIKit
 
 class GoalViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var startButton: UIButton!
+
+    
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        initVies()
     }
 
+    //MARK: - Overrides
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        goalTextView.endEditing(true)
+    }
+    
+    
+    //MARK: - Methods
+    private func initVies() {
+        goalTextView.backgroundColor = .white
+        goalTextView.layer.cornerRadius = 15
+        
+        startButton.layer.cornerRadius = 5
+        startButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
+        startButton.layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
+        startButton.layer.shadowOpacity = 1
+        startButton.layer.masksToBounds = false
+    }
 
 }
 
