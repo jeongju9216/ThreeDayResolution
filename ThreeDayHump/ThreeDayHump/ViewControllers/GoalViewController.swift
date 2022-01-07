97019ed0fa225c98fbf7aaafe1e84ebfa2e701c0
jeong9216 <dbwjdwn9216@gmail.com>
2022-01-07 22:25:20 +0900

    Goal Screen 마무리 작업 중

--- ThreeDayHump/ThreeDayHump/ViewControllers/GoalViewController.swift
+++ ThreeDayHump/ThreeDayHump/ViewControllers/GoalViewController.swift
@@ -31,8 +31,11 @@ class GoalViewController: UIViewController {
     
     //MARK: - Methods
     private func initVies() {
+        goalTextView.delegate = self
         goalTextView.backgroundColor = .white
         goalTextView.layer.cornerRadius = 15
+        goalTextView.textContainer.maximumNumberOfLines = 2
+        goalTextView.textContainer.lineBreakMode = .byTruncatingTail
         
         startButton.layer.cornerRadius = 5
         startButton.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).cgColor
@@ -43,3 +46,48 @@ class GoalViewController: UIViewController {
 
 }
 
+extension GoalViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        goalTextView.centerVerticalText()
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        goalTextView.centerVerticalText()
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        goalTextView.centerVerticalText()
+
+        
+        guard let text = textView.text else {
+            return
+        }
+        
+        let maxLength = 20
+        // 초과되는 텍스트 제거
+        if text.count >= maxLength {
+            let index = text.index(text.startIndex, offsetBy: maxLength)
+            let newString = text[text.startIndex..<index]
+            textView.text = String(newString)
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            goalTextView.endEditing(true)
+        }
+        
+        return true
+    }
+}
+
+extension UITextView {
+    func centerVerticalText() {
+        self.textAlignment = .center
+        let fitSize = CGSize(width: bounds.width, height: CGFloat.greatestFiniteMagnitude)
+        let size = sizeThatFits(fitSize)
+        let calculate = (bounds.size.height - size.height * zoomScale) / 2
+        let offset = max(1, calculate)
+        contentOffset.y = -offset
+    }
+}
