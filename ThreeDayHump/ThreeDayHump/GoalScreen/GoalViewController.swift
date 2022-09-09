424afe1e9300da78d6de61582e10a915a469ad2f
유정주 <dbwjdwn9216@gmail.com>
2022-09-09 10:18:58 +0900

    Add: final 추가

--- ThreeDayHump/ThreeDayHump/GoalScreen/GoalViewController.swift
+++ ThreeDayHump/ThreeDayHump/GoalScreen/GoalViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class GoalViewController: BaseViewController {
+final class GoalViewController: UIViewController {
 
     //MARK: - IBOutlets
     @IBOutlet weak var goalBackgroundView: UIView!
