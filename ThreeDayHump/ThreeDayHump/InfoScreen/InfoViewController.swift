6916173146e522d8c58ef3ce874ca244ee2cb333
유정주 <dbwjdwn9216@gmail.com>
2022-09-09 09:10:46 +0900

    Fix: 스토리보드 분리

--- ThreeDayHump/ThreeDayHump/InfoScreen/InfoViewController.swift
+++ ThreeDayHump/ThreeDayHump/InfoScreen/InfoViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
+class InfoViewController: BaseViewController {
 
     //MARK: - IBOutlets
     @IBOutlet weak var versionLabel: UILabel!
