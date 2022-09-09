959dfc95dcc1eff74be4f278382adb356f2075d5
유정주 <dbwjdwn9216@gmail.com>
2022-09-09 09:10:24 +0900

    Fix: 스토리보드 분리, 불필요한 메서드 이동

--- ThreeDayHump/ThreeDayHump/Base/BaseViewController.swift
+++ ThreeDayHump/ThreeDayHump/Base/BaseViewController.swift
@@ -8,34 +8,6 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-    func showGoalViewController() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let tabBarController = self.tabBarController
-        
-        let goalViewController = storyboard.instantiateViewController(withIdentifier: "GoalViewController")
-        
-        let navigation = storyboard.instantiateViewController(withIdentifier: "MoreNavigationController") as! UINavigationController
-        
-        tabBarController?.setViewControllers([goalViewController, navigation], animated: false)
-    }
-    
-    func showThreeDayViewController() {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let tabBarController = self.tabBarController
-        
-        let threeDayViewController = storyboard.instantiateViewController(withIdentifier: "ThreeDayViewController")
-        
-        let navigation = storyboard.instantiateViewController(withIdentifier: "MoreNavigationController") as! UINavigationController
-        
-        tabBarController?.setViewControllers([threeDayViewController, navigation], animated: false)
-    }
-    
-    
-    func resetUserDefaults() {
-        Goal.shared.goal = ""
-        Goal.shared.day = 0
-        
-        UserDefaults.standard.removeObject(forKey: "goal")
-        UserDefaults.standard.removeObject(forKey: "day")
-    }
+    static let storyboard: UIStoryboard = UIStoryboard(name: String(describing: BaseViewController.self), bundle: nil)
+    static let instantiate = storyboard.instantiateViewController(withIdentifier: String(describing: BaseViewController.self))
 }
