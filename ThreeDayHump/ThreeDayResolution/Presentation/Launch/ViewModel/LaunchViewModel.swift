f89ec9501337b841242e0579e724d55b7c330d2b
유정주 <dbwjdwn9216@gmail.com>
2023-06-29 13:55:24 +0900

    Launch 구현

---
+++ ThreeDayHump/ThreeDayResolution/Presentation/Launch/ViewModel/LaunchViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  LaunchViewModel.swift
+//  ThreeDayResolution
+//
+//  Created by 유정주 on 2023/06/29.
+//
+
+import Foundation
+
+struct LaunchViewModel {
+    
+    //MARK: - Properties
+    init() { }
+    
+    //MARK: - Methods
+    func launch() async -> StateData {
+        let serverStateData: StateData = await getServerStateData()
+        
+        let versionData: VersionData = await getVersionData()
+        setVersion(versionData)
+        
+        await fetchPolicyURL()
+        
+        return serverStateData
+    }
+    
+    private func getServerStateData() async -> StateData {
+        return await FirebaseService.shared.fetchState()
+    }
+    
+    private func getVersionData() async -> VersionData {
+        return await FirebaseService.shared.fetchVersion()
+    }
+    
+    private func setVersion(_ version: VersionData) {
+        BaseData.shared.currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+        
+        BaseData.shared.forcedUpdateVersion = version.forced
+        BaseData.shared.lastetVersion = version.lasted
+        
+        BaseData.shared.appleID = version.appleID
+        BaseData.shared.bundleID = version.bundleID
+    }
+    
+    private func fetchPolicyURL() async {
+        BaseData.shared.policyURL = await FirebaseService.shared.fetchPolicyURL()
+    }
+}
